4042049108cb21b2e73c08b2218fba0f21e9f80b
Murilo Sousa <murilosz085@gmail.com>
2026-05-22 12:29:12 -0300

    testes no FastAPI e configuração de database no psql

--- models.py
+++ models.py
@@ -1,3 +1,52 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import ChoiceType
 
-db = create_engine()
+db = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    STATUS_PEDIDOS =(
+        ("PENDENTE", "PENDENTE"),
+        ("CANCELADO","CANCELADO"),
+        ("FINALIZADO", "FINALIZADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS))
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    #itens = 
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+
+class ItemPedido(Base):
+    __tablename__ = "pedido_itens"
+
+    id =
+    quantidade =
+    sabor =
+    tamanho =
